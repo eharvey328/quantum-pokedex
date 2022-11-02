221f5fb206e43436ef87895df6d66e30e3ec657b
Eric Harvey <eric@harvey.io>
2022-11-02 03:46:26 -0400

    add basic search, filter, and pagination functionality (#3)

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,29 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { concatPagination } from "@apollo/client/utilities";
+
+import { QueryPokemonsArgs } from "./graphql/graphql";
 
 export const client = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_POKEDEX_ENDPOINT,
-  cache: new InMemoryCache(),
-  // ssrMode: typeof window === "undefined",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          pokemons: {
+            keyArgs: (args) => {
+              const { limit, offset, ...filters } = (args as QueryPokemonsArgs)
+                .query;
+              return JSON.stringify(filters);
+            },
+            merge: true,
+          },
+        },
+      },
+      PokemonConnection: {
+        fields: {
+          edges: concatPagination(),
+        },
+      },
+    },
+  }),
 });
